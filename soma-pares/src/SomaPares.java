649d3147908436a7d43731750b0a41e7c75939d3
Pedro Henrique <pedrohrezendeb07@gmail.com>
2024-09-20 11:00:31 -0300

    SomaPares feito

---
+++ soma-pares/src/SomaPares.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SomaPares {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de números que você irá inserir: ");
+        int quantidadeNumeros = scanner.nextInt();
+
+        int somaPares = 0;
+        int armazenaNumeros;
+
+        for(int i = 1; i <= quantidadeNumeros; i++) {
+            System.out.println("Digite o " + i + "º  número");
+            armazenaNumeros = scanner.nextInt();
+
+            if(armazenaNumeros % 2 == 0) {
+                somaPares += armazenaNumeros;
+            }
+        }
+
+        System.out.println("Resultado da soma dos números pares -> " + somaPares);
+        scanner.close();
+    }
+}
